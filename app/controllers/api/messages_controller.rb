58e86a5b510d0114b92fb040d6efa09201056b19
masouma <masouma2020@gmail.com>
2023-03-08 09:14:50 +0430

    fix linter error

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
-    def random
-      greetings = Message.all.sample(:text)
-      render json: greetings
-    end
+  def random
+    greetings = Message.all.sample(:text)
+    render json: greetings
   end
+end
