fd9432b78bffa4cb9ca33de06f02e1979386150e
masouma <masouma2020@gmail.com>
2023-03-08 09:13:07 +0430

    UPdate readme file

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
     def random
-      greetings = Message.all.sample.slice(:text)
+      greetings = Message.all.sample(:text)
       render json: greetings
     end
   end
